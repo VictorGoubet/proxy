331878feb33429c52975f27c055e0923cddc7cef
Victor Goubet <victorgoubet@orange.fr>
2022-03-11 15:20:54 +0100

    add routes

--- app.js
+++ app.js
@@ -10,12 +10,16 @@ app.use(cors());
 
 // call the API
 
-app.get("/info", (req, res) => {
+app.get("/", (req, res) => {
     res.send('Hey');
   });
 
 
-app.get("/", (req, res) => {
+app.get("/access2", (req, res) => {
+    res.redirect('https://www.linkedin.com/badges/profile/create?vanityname=gad-azeraf&preferredlocale=en_US&trk=public_profile-settings_badge')
+  });
+
+app.get("/access", (req, res) => {
   axios.get("https://www.linkedin.com/badges/profile/create?vanityname=gad-azeraf&preferredlocale=en_US&trk=public_profile-settings_badge").then((data) => {
       res.send(data);
     }).catch((error) => {
