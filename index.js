5af697fec742206e7bb7a1751b601c654bbb9f0b
Victor Goubet <victorgoubet@orange.fr>
2022-03-11 15:08:35 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const axios = require("axios");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+// call the API
+app.get("/", (req, res) => {
+  axios.get("https://www.linkedin.com/badges/profile/create?vanityname=gad-azeraf&preferredlocale=en_US&trk=public_profile-settings_badge").then((data) => {
+      res.send(data);
+    }).catch((error) => {
+      console.log(error.message);
+    });
+});
+
+
+app.listen(3000, () => console.log("server started"));
