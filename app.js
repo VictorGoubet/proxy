c59dd117fee049bfa85efeb4ed2526e3fda032fc
Victor Goubet <victorgoubet@orange.fr>
2022-03-11 15:13:10 +0100

    add procfile

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 // call the API
+
+app.get("/info", (req, res) => {
+    res.send('Hey');
+  });
+
+
 app.get("/", (req, res) => {
   axios.get("https://www.linkedin.com/badges/profile/create?vanityname=gad-azeraf&preferredlocale=en_US&trk=public_profile-settings_badge").then((data) => {
       res.send(data);
