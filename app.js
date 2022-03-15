f15dc5d88af55641f1fd1858756e9e3128b2a4a2
Victor Goubet <victorgoubet@orange.fr>
2022-03-15 10:07:18 +0100

    add absolute path

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
-
+const path = require('path')
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
@@ -10,7 +10,7 @@ app.use(cors());
 // call the API
 
 app.get("/", (req, res) => {
-    res.sendFile('index.html');
+    res.sendFile(path.join(__dirname, 'index.html'));
   });
 
 port = process.env.PORT || 80
